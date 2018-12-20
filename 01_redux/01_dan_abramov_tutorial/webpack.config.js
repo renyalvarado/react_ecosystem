c4ca9a1dc34c7a9245b2e94c5ca112e4e79644de
renyalvarado <renyalvarado@gmail.com>
2018-12-20 20:08:09 -0300

    Add React using webpack

--- 01_redux/01_dan_abramov_tutorial/webpack.config.js
+++ 01_redux/01_dan_abramov_tutorial/webpack.config.js
@@ -7,6 +7,18 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [
+      {
+        // this is so that we can compile any React,
+        // ES6 and above into normal ES5 syntax
+        test: /\.(js|jsx)$/,
+        // we do not want anything from node_modules to be compiled
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       favicon: 'public/favicon.ico',
