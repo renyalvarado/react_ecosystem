6a2e0e816a25dda2a7dcbb4b95ffe712409a9a88
renyalvarado <renyalvarado@gmail.com>
2018-12-20 18:15:51 -0300

    Add Redux

--- 01_redux/01_dan_abramov_tutorial/src/index.js
+++ 01_redux/01_dan_abramov_tutorial/src/index.js
@@ -2,27 +2,7 @@
 'use strict';
 
 import {counter} from './counter';
-
-const createStore = (reducer) => {
-  let state;
-  let listeners = [];
-  const getState = () => state;
-
-  const dispatch = (action) => {
-    state = reducer(state, action);
-    listeners.forEach(listener => listener());
-  };
-
-  const subscribe = (listener) => {
-    listeners.push(listener);
-    return () => {
-      listeners =  listeners.filter(l => l !== listener);
-    };
-  };
-
-  dispatch({});
-  return {getState, dispatch, subscribe};
-};
+import {createStore} from "redux";
 
 const scoreText = document.querySelector('#score');
 const store = createStore(counter);
