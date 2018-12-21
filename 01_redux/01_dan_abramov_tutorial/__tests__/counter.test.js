8dae277d10b100950c4c4ddb585051d84e03ba14
renyalvarado <renyalvarado@gmail.com>
2018-12-21 19:43:21 -0300

    Add add, remove and increment functions to counter

--- 01_redux/01_dan_abramov_tutorial/__tests__/counter.test.js
+++ 01_redux/01_dan_abramov_tutorial/__tests__/counter.test.js
@@ -1,6 +1,10 @@
-import { counter } from '../src/counter';
+/* eslint-env es6 */
+'use strict';
 
-test('Counter component', () => {
+import { counter, addCounter, removeCounter, incrementCounter } from '../src/counter';
+import deepFreeze from 'deep-freeze';
+
+test('Counter function', () => {
   expect(counter(0, { type: 'INCREMENT' })).toBe(1);
   expect(counter(1, { type: 'INCREMENT' })).toBe(2);
   expect(counter(2, { type: 'INCREMENT' })).toBe(3);
@@ -9,4 +13,25 @@ test('Counter component', () => {
   expect(counter(0, { type: 'DECREMENT' })).toBe(-1);
   expect(counter(0, { type: 'OTHER_ACTION' })).toBe(0);
   expect(counter(undefined, {})).toBe(0);
+});
+
+test('Add counter function', () => {
+  const listBefore = [];
+  const listAfter = [0];
+  deepFreeze(listBefore);
+  expect(addCounter(listBefore)).toEqual(listAfter);
+});
+
+test('Remove counter function', () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 20];
+  deepFreeze(listBefore);
+  expect(removeCounter(listBefore, 1)).toEqual(listAfter);
+});
+
+test('Increment counter function', () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 11, 20];
+  deepFreeze(listBefore);
+  expect(incrementCounter(listBefore, 1)).toEqual(listAfter);
 });
--- 01_redux/01_dan_abramov_tutorial/src/counter.js
+++ 01_redux/01_dan_abramov_tutorial/src/counter.js
@@ -11,3 +11,22 @@ export const counter = (state = 0, action) => {
       return state;
   }
 };
+
+export const addCounter = (list) => {
+  return [...list, 0];
+};
+
+export const removeCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    ...list.slice(index + 1)
+  ];
+};
+
+export const incrementCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    list[index] + 1,
+    ...list.slice(index + 1)
+  ];
+};
