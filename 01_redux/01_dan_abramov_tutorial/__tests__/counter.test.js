ac2183e9416a5800c0381040ee47d720442aac1c
renyalvarado <renyalvarado@gmail.com>
2018-12-21 18:40:19 -0300

    Install Eslint Jes's plugin

--- 01_redux/01_dan_abramov_tutorial/__tests__/counter.test.js
+++ 01_redux/01_dan_abramov_tutorial/__tests__/counter.test.js
@@ -1,12 +1,12 @@
-import {counter} from '../src/counter';
+import { counter } from '../src/counter';
 
 test('Counter component', () => {
-  expect(counter(0, {type: 'INCREMENT'})).toBe(1);
-  expect(counter(1, {type: 'INCREMENT'})).toBe(2);
-  expect(counter(2, {type: 'INCREMENT'})).toBe(3);
-  expect(counter(2, {type: 'DECREMENT'})).toBe(1);
-  expect(counter(1, {type: 'DECREMENT'})).toBe(0);
-  expect(counter(0, {type: 'DECREMENT'})).toBe(-1);
-  expect(counter(0, {type: 'OTHER_ACTION'})).toBe(0);
+  expect(counter(0, { type: 'INCREMENT' })).toBe(1);
+  expect(counter(1, { type: 'INCREMENT' })).toBe(2);
+  expect(counter(2, { type: 'INCREMENT' })).toBe(3);
+  expect(counter(2, { type: 'DECREMENT' })).toBe(1);
+  expect(counter(1, { type: 'DECREMENT' })).toBe(0);
+  expect(counter(0, { type: 'DECREMENT' })).toBe(-1);
+  expect(counter(0, { type: 'OTHER_ACTION' })).toBe(0);
   expect(counter(undefined, {})).toBe(0);
 });
