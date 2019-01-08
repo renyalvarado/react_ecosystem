7f2360b78a2b95bfdb0502f60b3221623b414859
renyalvarado <renyalvarado@gmail.com>
2019-01-08 18:17:13 -0300

    Passing the store down with Provider from React Redux (v 5.1.1)

--- 01_redux/01_dan_abramov_tutorial/src/index.js
+++ 01_redux/01_dan_abramov_tutorial/src/index.js
@@ -4,11 +4,11 @@
 import { createStore, combineReducers } from 'redux';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { Provider } from 'react-redux';
 import { counter } from './counter';
 import { todos, visibilityFilter } from './todos';
 import Counter from './components/counter';
 import Todo from './components/todo';
-import PropTypes from 'prop-types';
 
 const store = createStore(combineReducers({ counter, todos, visibilityFilter }));
 const renderCounter = () => {
@@ -21,26 +21,6 @@ const renderCounter = () => {
         store.dispatch({ type: 'DECREMENT' });
       }} />,
     document.getElementById('root'));
-};
-
-class Provider extends React.Component {
-  getChildContext () {
-    return {
-      store: this.props.store
-    };
-  }
-  render () {
-    return this.props.children;
-  }
-}
-
-Provider.propTypes = {
-  store: PropTypes.object.isRequired,
-  children: PropTypes.node.isRequired
-};
-
-Provider.childContextTypes = {
-  store: PropTypes.object
 };
 
 ReactDOM.render(
