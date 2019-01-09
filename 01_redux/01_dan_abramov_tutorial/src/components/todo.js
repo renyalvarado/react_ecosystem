134a0618261139b92c5c23bb47cd931bbb78d1f4
renyalvarado <renyalvarado@gmail.com>
2019-01-09 19:03:44 -0300

    GeneratecContainers with connect() from React Redux (FooterLink)

--- 01_redux/01_dan_abramov_tutorial/src/components/todo.js
+++ 01_redux/01_dan_abramov_tutorial/src/components/todo.js
@@ -24,39 +24,23 @@ Link.propTypes = {
   onClick: PropTypes.func.isRequired
 };
 
-class FilterLink extends React.Component {
-  componentDidMount () {
-    const { store } = this.context;
-    this.unsubscribe = store.subscribe(() => this.forceUpdate());
-  }
-  componentWillUnmount () {
-    this.unsubscribe();
-  }
-  render () {
-    const { store } = this.context;
-    const { filter, children } = this.props;
-    const state = store.getState();
-    return (
-      <Link active={filter === state.visibilityFilter} onClick={() => {
-        store.dispatch({
-          type: 'SET_VISIBILITY_FILTER',
-          filter: filter
-        });
-      }}>
-        {children}
-      </Link>
-    );
-  }
-}
-
-FilterLink.propTypes = {
-  filter: PropTypes.string.isRequired,
-  children: PropTypes.node.isRequired
+const mapStateToLinkProps = (state, ownProps) => {
+  return {
+    active: ownProps.filter === state.visibilityFilter
+  };
 };
 
-FilterLink.contextTypes = {
-  store: PropTypes.object
+const mapDispatchToLinkProps = (dispatch, ownProps) => {
+  return {
+    onClick: () => {
+      dispatch({
+        type: 'SET_VISIBILITY_FILTER',
+        filter: ownProps.filter
+      });
+    }
+  };
 };
+const FilterLink = connect(mapStateToLinkProps, mapDispatchToLinkProps)(Link);
 
 const Footer = () => (
   <p>
