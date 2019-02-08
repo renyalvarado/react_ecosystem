e5f1ef878ac1bb89d28ff253ceae97987bca0dad
renyalvarado <renyalvarado@gmail.com>
2019-02-08 12:10:57 -0300

    Move actions to it own directory

--- 01_redux/01_dan_abramov_tutorial/src/components/todo.js
+++ 01_redux/01_dan_abramov_tutorial/src/components/todo.js
@@ -4,7 +4,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { addTodo, setVisibilityFilter, toggleTodo } from './actions';
+import { addTodo, setVisibilityFilter, toggleTodo } from '../actions/actions';
 
 const Link = ({ active, children, onClick }) => {
   if (active) {
