92ea3bf8f56b21e7d26bb5e845d00c915ee33445
renyalvarado <renyalvarado@gmail.com>
2019-02-11 11:59:20 -0300

    Normalize the State Shape

--- 01_redux/01_dan_abramov_tutorial/src/reducers/todos.js
+++ 01_redux/01_dan_abramov_tutorial/src/reducers/todos.js
@@ -1,6 +1,8 @@
 /* eslint-env es6 */
 'use strict';
 
+import { combineReducers } from 'redux';
+
 const todo = (state, action) => {
   switch (action.type) {
     case ('ADD_TODO'):
@@ -14,33 +16,48 @@ const todo = (state, action) => {
         return { ...state, completed: !state.completed };
       }
       return state;
-  }
-};
-
-const todos = (state = [], action) => {
-  switch (action.type) {
-    case ('ADD_TODO'):
-      return [
-        ...state,
-        todo(undefined, action)
-      ];
-    case ('TOGGLE_TODO'):
-      return state.map(t => todo(t, action));
     default:
       return state;
   }
 };
 
+const byId = (state, action) => {
+  switch (action.type) {
+    case ('ADD_TODO'):
+    case ('TOGGLE_TODO'):
+      return {
+        ...state,
+        [action.id]: todo(state[action.id], action)
+      };
+    default:
+      return state;
+  }
+};
+
+const allIds = (state, action) => {
+  switch (action.type) {
+    case ('ADD_TODO'):
+      return [ ...state, action.id ];
+    default:
+      return state;
+  }
+};
+
+const todos = combineReducers({ byId, allIds });
+
 export default todos;
 
+const getAllTodos = (state) => state.allIds.map(id => state.byId[id]);
+
 export const getVisibleTodos = (state, filter) => {
+  const allTodos = getAllTodos(state);
   switch (filter) {
     case 'all':
-      return state;
+      return allTodos;
     case 'completed':
-      return state.filter(t => t.completed);
+      return allTodos.filter(t => t.completed);
     case 'active':
-      return state.filter(t => !t.completed);
+      return allTodos.filter(t => !t.completed);
     default:
       throw new Error(`Unknown filter: ${filter}.`);
   }
