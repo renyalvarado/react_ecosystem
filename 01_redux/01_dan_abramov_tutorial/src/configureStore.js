74b2aee5819613c37a5020430ce6f258d9fb74d4
renyalvarado <renyalvarado@gmail.com>
2019-02-11 12:50:16 -0300

    Wrapping dispatch() to Log Actions

--- 01_redux/01_dan_abramov_tutorial/src/configureStore.js
+++ 01_redux/01_dan_abramov_tutorial/src/configureStore.js
@@ -5,6 +5,23 @@ import { createStore } from 'redux';
 import { loadState, saveState } from './localStorage';
 import allReducers from './reducers';
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+  if (!console.group) {
+
+    return rawDispatch;
+  }
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const returnValue = rawDispatch(action);
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
+
 const configureStore = () => {
   const preLoadedState = loadState();
   /* eslint-disable no-underscore-dangle */
@@ -12,6 +29,9 @@ const configureStore = () => {
     preLoadedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store);
+  }
   /* eslint-enable */
   store.subscribe(throttle(() => saveState({
     counter: store.getState().counter,
