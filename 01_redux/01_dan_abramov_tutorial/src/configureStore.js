142e6277943298d11810908b19fd5ce92b7d5c9f
renyalvarado <renyalvarado@gmail.com>
2019-02-11 18:29:12 -0300

    Implement middleware chain

--- 01_redux/01_dan_abramov_tutorial/src/configureStore.js
+++ 01_redux/01_dan_abramov_tutorial/src/configureStore.js
@@ -3,42 +3,44 @@
 import { createStore } from 'redux';
 import allReducers from './reducers';
 
-const addLoggingToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
+const logger = (store) => (next) => {
   if (!console.group) {
-    return rawDispatch;
+    return next;
   }
   return (action) => {
     console.group(action.type);
     console.log('%c prev state', 'color: gray', store.getState());
     console.log('%c action', 'color: blue', action);
-    const returnValue = rawDispatch(action);
+    const returnValue = next(action);
     console.log('%c next state', 'color: green', store.getState());
     console.groupEnd();
     return returnValue;
   };
 };
-
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  return (action) => {
-    if (typeof action.then === 'function') {
-      return action.then(rawDispatch);
-    }
-    return rawDispatch(action);
-  };
+// eslint-disable-next-line unused-parameter
+const promise = (store) => (next) => (action) => {
+  if (typeof action.then === 'function') {
+    return action.then(next);
+  }
+  return next(action);
 };
 
+const wrapDispatchWithMiddlewares = (store, middlewares) =>
+  middlewares.slice().reverse().forEach(middleware => {
+    store.dispatch = middleware(store)(store.dispatch);
+  });
+
 const configureStore = () => {
+  const middlewares = [promise];
   /* eslint-disable no-underscore-dangle */
   const store = createStore(allReducers,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(logger);
   }
   /* eslint-enable */
-  store.dispatch = addPromiseSupportToDispatch(store);
+  wrapDispatchWithMiddlewares(store, middlewares);
   return store;
 };
 
