7ddc4939f651d63e2d21ce308d4cf7272e39c5db
renyalvarado <renyalvarado@gmail.com>
2019-02-11 14:50:01 -0300

    Add a fake backend to the project

--- 01_redux/01_dan_abramov_tutorial/src/configureStore.js
+++ 01_redux/01_dan_abramov_tutorial/src/configureStore.js
@@ -1,14 +1,11 @@
 /* eslint-env es6 */
 'use strict';
-import throttle from 'lodash.throttle';
 import { createStore } from 'redux';
-import { loadState, saveState } from './localStorage';
 import allReducers from './reducers';
 
 const addLoggingToDispatch = (store) => {
   const rawDispatch = store.dispatch;
   if (!console.group) {
-
     return rawDispatch;
   }
   return (action) => {
@@ -23,21 +20,14 @@ const addLoggingToDispatch = (store) => {
 };
 
 const configureStore = () => {
-  const preLoadedState = loadState();
   /* eslint-disable no-underscore-dangle */
   const store = createStore(allReducers,
-    preLoadedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
   if (process.env.NODE_ENV !== 'production') {
     store.dispatch = addLoggingToDispatch(store);
   }
   /* eslint-enable */
-  store.subscribe(throttle(() => saveState({
-    counter: store.getState().counter,
-    todos: store.getState().todos
-  }), 1000));
-
   return store;
 };
 
